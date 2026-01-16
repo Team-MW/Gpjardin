daacec5a26d49a095e90b724b909cd657e3cd24e
sofianelamine772-design <mwcrea.agency@gmail.com>
2026-01-16 16:45:43 +0100

    dd

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="container">
                 <div className="footer-top">
                     <div className="footer-grid">
-                        {/*   ddBranding */}
+                        {/*     dd. Branding */}
                         <div className="footer-col branding-col">
                             <Link to="/" className="footer-logo-link">
                                 <img
